28cc928894ad9f4fa39e87d1f8e57cb4753e80c2
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-05-19 01:59:07 -0700

    setup handlebars, created homepage, added stylesheet, template routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -51,23 +51,8 @@ router.get('/:id', (req, res) => {
       ],
       include: [
         {
-          model: Post,
-          attributes: ['id', 'title', 'post_url', 'created_at']
-        },
-        // include the Comment model here:
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'created_at'],
-          include: {
-            model: Post,
-            attributes: ['title']
-          }
-        },
-        {
-          model: Post,
-          attributes: ['title'],
-          through: Vote,
-          as: 'voted_posts'
+          model: User,
+          attributes: ['username']
         }
       ]
     })
